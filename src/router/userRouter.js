2795eab8c751e181fa849a7fbb42f3a72166abd0
Halid Ünal <halidunal@gmail.com>
2024-05-14 11:19:05 +0300

    user router

---
+++ src/router/userRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/getUsers", (req, res) => {});
+
+router.get("/getUser/:userId", (req, res) => {});
+
+module.exports = {
+  userRouter: router,
+};
