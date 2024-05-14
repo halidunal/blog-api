16c8ba78ca018d2572ae5ed55badd5888a3a2cb4
Halid Ünal <halidunal@gmail.com>
2024-05-14 14:23:21 +0300

    models

---
+++ src/models/blogModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    reqired: true,
+  },
+  file: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+  fav_count: {
+    type: Number,
+  },
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "user",
+  },
+  tag: [
+    {
+      type: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("blog", BlogSchema);
---
+++ src/models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    reqired: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    reqired: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  blogs: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+module.exports = mongoose.model("user", UserSchema);
