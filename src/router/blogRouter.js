08130e468040150bd0442a7cce6bf972cf425bdf
Halid Ünal <halidunal@gmail.com>
2024-05-14 11:17:57 +0300

    blog router

---
+++ src/router/blogRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/getBlogs", (req, res) => {});
+
+router.get("/getBlog/:blogId", (req, res) => {});
+
+module.exports = {
+  blogRouter: router,
+};
