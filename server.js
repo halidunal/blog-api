e996837f91566bdd58996e83c56641674b2273cd
Halid Ünal <halidunal@gmail.com>
2024-05-14 17:16:50 +0300

    swagger

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const app = express();
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 const routes = require("./src/routers/index");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./src/config/swagger.json");
 const db = require("./src/db/index");
+
 dotenv.config();
+
 app.use(helmet());
 app.use(express.json());
-app.use(process.env.API_PREFIX + "/user", routes.userRouter);
-app.use(process.env.API_PREFIX + "/blog", routes.blogRouter);
+
+app.use(process.env.APP_API_PREFIX + "user", routes.userRouter);
+app.use(process.env.APP_API_PREFIX + "blog", routes.blogRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 db();
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
